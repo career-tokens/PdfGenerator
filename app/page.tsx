7230458a330807fa265a739c977dcb1023c9d935
mainak <heellomeelo@gmail.com>
2024-04-11 10:03:12 +0530

    small change

--- app/page.tsx
+++ app/page.tsx
@@ -70,7 +70,7 @@ const Home: React.FC<HomeProps> = () => {
               Currently available templates:
             </div>
             <div className="available-templates flex flex-wrap w-[300px] sm:w-[500px] justify-center gap-x-[2vw] gap-y-[2vh] text-center">
-              <Link href="/tigercv" onClick={() => { 
+              {/* <Link href="/tigercv" onClick={() => { 
                 toast.warning("Currently this CV takes a lot of server time and hence during generation, it might get timed out.",
                 {
                   className: 'text-base  w-[400px] flex justify-center',
@@ -80,7 +80,7 @@ const Home: React.FC<HomeProps> = () => {
                 <button className="shadow-[0_4px_14px_0_rgb(0,0,0,10%)] hover:shadow-[0_6px_20px_rgba(93,93,93,23%)] px-8 py-2 bg-[#fff] text-black rounded-md font-semibold transition duration-200 ease-linear">
                   Tiger Abrodi&apos;s CV
                 </button>
-              </Link>
+              </Link> */}
               <Link href="/cv">
                 <button className="shadow-[0_4px_14px_0_rgb(0,0,0,10%)] hover:shadow-[0_6px_20px_rgba(93,93,93,23%)] px-8 py-2 bg-[#fff] text-black rounded-md font-semibold transition duration-200 ease-linear">
                   CV
