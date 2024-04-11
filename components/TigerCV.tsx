5a10f1093c6c03d4fe2bdfb8c0c7792454a257a0
mainak <heellomeelo@gmail.com>
2024-04-12 01:30:57 +0530

    small changes for proper length of PDF

--- components/TigerCV.tsx
+++ components/TigerCV.tsx
@@ -11,7 +11,7 @@ export const TigerCV:React.FC<TigerCVDataProps> = ({data}) => {
       {
         String.raw`@import url('https://fonts.googleapis.com/css2?family=Libre+Baskerville&family=Literata:opsz@7..72&family=Lora&family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&family=Montserrat&family=Mulish&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto&family=Varela+Round&display=swap');
         @page{
-          size:10in 17in;
+          size:10in 18in;
           margin:0;
         }`
       }
