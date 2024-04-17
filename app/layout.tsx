45e314872f4c16d76f8377e599125e7c28e4d840
mainak <heellomeelo@gmail.com>
2024-04-17 10:14:39 +0530

    added google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,7 @@ interface RootLayoutProps {
 const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
   return (
     <html lang="en">
+       <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS} />
       <body className={inter.className}>{children}</body>
       <Toaster richColors position='top-center' closeButton expand visibleToasts={1}/>
     </html>
