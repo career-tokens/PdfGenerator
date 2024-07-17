d07997882b0071d571e480eb8b96ab110fcfc223
mainak <heellomeelo@gmail.com>
2024-07-17 13:23:45 +0530

    making the design resume but for now commented

---
+++ app/designCV/initialData.ts
@@ -0,0 +1,107 @@
+export const designInitialData = {
+    name: "Rohit Kumar",
+    profession: "UI/Visual Design Wizard",
+    profile:`<p>I am an aspiring UI/Visual Designer with impeccable attention to detail. I have a total of 8 years of experience, ranging from graphic design, UX writing, wireframing, front-end development and most importantly, UI/Visual designing.</p>
+<p><br></p>
+<p>My primary focus throughout, has been to create -</p>
+<p><br></p>
+<p><strong>• Data-driven UI design</strong></p>
+<p><br></p>
+<p><strong>• Adaptable design systems across standalone &amp; web-based</strong></p>
+<p><br></p>
+<p>• <strong>Responsive, multi-screen UI design</strong></p>`,
+    experience: [{
+        position: `<p>• Design Lead<strong>, XYZ Company</strong></p>`,
+        timeline:"October 2020-present"
+    },
+        {
+            position: `<p>• Associate Designer<strong>, ABD Solutions</strong></p>`,
+            timeline:"March 2017-September 2020"
+        }, {
+            position: `<p>• Graphic Designer<strong>, QRS InfoSolutions</strong></p>`,
+            timeline:"February 2014-February 2017"
+        }],
+    email: "email@website.com",
+    emailRequired: true,
+    phone: "+91 91281 23232",
+    phoneRequired: true,
+    behance: "www.behance.com/myPortfolio",
+    behanceRequired: true,
+    linkedin: "www.linkedin.com/in.myProfile",
+    linkedinReq: true,
+    skills: [{
+        title: "Wireframing",
+        stars:5
+    },
+        {
+            title: "UI Design",
+            stars:4
+        },
+        {
+            title: "Icon Design",
+            stars:3
+        },
+        {
+            title: "Design Systems",
+            stars:4
+        },
+        {
+            title: "Motion Graphics",
+            stars:3
+        }],
+    software: ["Figma", "Adobe After Effects", "Principle", "Adobe Illustrator", "Blender"],
+    academics: [{
+        course: "Bachelor of Design",
+        whenAndWhere: "2014,XYZ Institute of Arts, Bangalore",
+    }, {
+        course: "UX Design Mastery",
+        whenAndWhere:"2018,www.coursewebsite.com"
+        }],
+    interests: [{
+        name: "YouTube Content for Designers",
+        link:"youtube.com/myChannelName"
+    },
+        {
+            name: "50+ potrait sketches",
+            link:"www.artstation.com/mySketches"
+        }, {
+            name: "Creator of indie comics",
+            link:"Mr. Officeboss Doodles"
+        }],
+    languages: ["English", "Hindi", "Kannada"],
+    languagesRequired:true,
+    hobbies: ["Trekking", "Playing the Piano", "Sketching potraits"],
+    hobbiesRequired: true,
+    notableWork: [{
+        name: "Immaterial Design",
+        timeline:"January 2022",
+        description: "A comprehensive Design System",
+        features: [
+            "Created multiscreen/multiplatform design systems",
+            "Used in 4 internal and 2 customer facing projects",
+            "I was one of the 3 people on the project, with 60-70% ownership",
+            "Experimented on auto-convert Figma to Javascript workflow(currently in progress)."
+        ]
+    }, {
+        name: "The Y Monk",
+        timeline:"October 2021",
+        description: "Motion Graphics Library for desktop web",
+        features: [
+            "Conceptualised and created a fully automated motion graphics library",
+            "primarily targeted towards desktop web app design",
+            "Sole creator of the product",
+            "Total of customer downloads of 140000+ in the first 6 months",
+            "Successfully adapted and implemented in 45 projects in the last 1 year"
+        ]
+        },
+        {
+            name: "Web + Mobile App Designs",
+            timeline:"September 2021",
+            description: "",
+            features: ["Worked on more than 7 web app designs",
+                "Created 3 mobile app UI designs and wireframes(2 of which are live)",
+                "Worked with 3 other designers to create prototypes for 12 mobile apps",
+                "Implemented wordress theme designs for 2 multiscreen projects"
+            ]
+    }]
+}
